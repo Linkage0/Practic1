f8ee3d626d6a0c214841e462f73f54eda45da5c5
Linkage0 <linkagenexus@gmail.com>
2021-04-19 09:48:36 +0300

    Edit Product Class!

--- PR1/Product.cs
+++ PR1/Product.cs
@@ -11,5 +11,63 @@ namespace PR1
         private string name;
         private string cipher;
         private int amount;
+
+        Product()
+        {
+            name = "";
+            cipher = "";
+            amount = 0;
+        }
+
+        public void ShowName()
+        {
+            Console.WriteLine("The current stuff name = " + name);
+        }
+
+        public void ShowCipher()
+        {
+            Console.WriteLine("The current stuff cipher = " + cipher);
+        }
+
+        public void ShowAmount()
+        {
+            Console.WriteLine("The current stuff amount = " + amount);
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Cipher
+        {
+            get
+            {
+                return cipher;
+            }
+            set
+            {
+                cipher = value;
+            }
+        }
+
+        public int Amount
+        {
+            get
+            {
+                return amount;
+            }
+            set
+            {
+                amount = value;
+            }
+        }
     }
 }
