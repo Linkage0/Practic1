85e332fa53a27e60c615974a6b4b6133ace1d7b1
Linkage0 <linkagenexus@gmail.com>
2021-04-19 09:39:24 +0300

    Create Project!

---
+++ PR1/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR1
+{
+    class Product
+    {
+        private string name;
+        private string cipher;
+        private int amount;
+    }
+}
